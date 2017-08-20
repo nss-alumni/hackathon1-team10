7bc7cc8ddd3d4bbbcdc104affe94ab2bea56944c
Joshua Meyers <joshua.r.meyers@gmail.com>
2017-08-20 12:52:48 -0500

    add auth service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MdButtonModule, MdCheckboxModule} from '@angular/material';
 import { NavbarComponent } from './navbar/navbar.component';
+import {AuthService} from './auth.service';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,7 @@ import { NavbarComponent } from './navbar/navbar.component';
     MdButtonModule,
     MdCheckboxModule
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
