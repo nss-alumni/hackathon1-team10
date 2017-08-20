350f66ecfb5ce8ce3db2b978f76680cd27f05337
Jordan Dhaenens <jordan.dhaenens@gmail.com>
2017-08-20 15:32:46 -0500

    ready to merge to master

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,14 +1,18 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
+import "rxjs/add/operator/map";
 import { AngularFireAuth } from 'angularfire2/auth';
 import * as firebase from 'firebase/app';
 
 @Injectable()
 export class AuthService {
   user: Observable<firebase.User>;
-  
+  isAuthenticated: Observable<boolean>;
+
   constructor(private afAuth: AngularFireAuth) {
-    this.user = afAuth.authState;
+    this.user = afAuth.idToken;
+    this.isAuthenticated = afAuth.idToken
+      .map(user => user ? true : false);
   }
 
   login() {
