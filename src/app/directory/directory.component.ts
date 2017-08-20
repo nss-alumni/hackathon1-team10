e3144614f4fdeac8cb28d27b3e4b8c943b63a789
Benjamin Wilburn <bwilburnstrength@gmail.com>
2017-08-20 17:31:31 -0500

    git alumni directory cards

--- src/app/directory/directory.component.ts
+++ src/app/directory/directory.component.ts
@@ -16,7 +16,7 @@ export class DirectoryComponent implements OnInit {
       {
         name: 'Brooke Wittenberg',
         cohort: 'Cohort 21',
-        image: 'brooke.gif',
+        image: 'brooke.jpeg',
         skills: ['HTML', 'CSS', 'SCSS', 'JS', 'JQuery'],
         contact: 'brooke.wittenberg@gmail.com',
         bio: 'I am a current student eagerly absorbing all of the material heaped upon me.'
@@ -24,7 +24,7 @@ export class DirectoryComponent implements OnInit {
       {
         name: 'Ben Wilburn',
         cohort: 'Cohort 13',
-        image: 'ben.jpg',
+        image: 'ben.jpeg',
         skills: ['HTML', 'CSS', 'SCSS', 'JS', 'JQuery', 'React', 'All the things'],
         contact: 'bwilburnstrength@gmail.com',
         bio: 'I am a front end developer and I am awesome, obviously.'
@@ -32,7 +32,7 @@ export class DirectoryComponent implements OnInit {
       {
         name: 'Brian Hiatt',
         cohort: 'Cohort 6',
-        image: 'brian.jpg',
+        image: 'brian.png',
         skills: ['HTML', 'CSS', 'SCSS', 'JS', 'JQuery', 'Angular', 'Typescript', 'All the things'],
         contact: 'youcantry@gmail.com',
         bio: 'I am the greatest leader of all time.'
@@ -40,7 +40,7 @@ export class DirectoryComponent implements OnInit {
       {
         name: 'Jordan Dhaenens',
         cohort: 'Cohort 19',
-        image: 'jordan.png',
+        image: 'jordan.jpeg',
         skills: ['HTML', 'CSS', 'SCSS', 'JS', 'JQuery', 'Angular', 'And Stuff'],
         contact: 'dapperdan@gmail.com',
         bio: 'I love NSS, kefir, facial hair, and long strolls while wearing suspenders.'
@@ -48,7 +48,7 @@ export class DirectoryComponent implements OnInit {
       {
         name: 'Joshua Meyers',
         cohort: 'Cohort 20',
-        image: 'josh.jpg',
+        image: 'joshua.jpeg',
         skills: ['HTML', 'CSS', 'SCSS', 'JS', 'JQuery', 'Angular', 'And Stuff'],
         contact: 'keepinitcool@gmail.com',
         bio: 'I love NSS, kefir, facial hair, and long strolls while wearing suspenders.'
@@ -56,4 +56,7 @@ export class DirectoryComponent implements OnInit {
     ];
   }
 
+  getAssetUrl(filename) {
+      return `assets/${filename}`;
+  }
 }
