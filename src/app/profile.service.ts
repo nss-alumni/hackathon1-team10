35c656fa52629357aa862f29cef16a52b50cc3c6
Jordan Dhaenens <jordan.dhaenens@gmail.com>
2017-08-20 16:51:35 -0500

    swtiching branches

--- src/app/profile.service.ts
+++ src/app/profile.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
 import { AuthService } from './auth.service';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
 @Injectable()
 export class ProfileService {
-  
+
 
   constructor(private profile: AuthService) { 
 
